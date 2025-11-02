ee89c586579a624e42a8f7eeb45e83a218160304
shinospp <ppshinos@gmail.com>
2025-11-02 15:50:21 +0530

    Initial commit

---
+++ src/test/java/com/spp/jenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.spp.jenkins.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	
+	static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing.....");
+		assertEquals(true, true);
+	}
+
+}
