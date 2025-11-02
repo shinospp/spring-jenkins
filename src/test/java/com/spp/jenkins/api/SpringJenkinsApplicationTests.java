986723c6555c6ea7b9479c19077bc14cda3a3a5d
shinospp <ppshinos@gmail.com>
2025-11-02 16:35:41 +0530

    third commit

--- src/test/java/com/spp/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/spp/jenkins/api/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
 
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("Test case executing.....");
 		logger.info("Test case executing second log statement....."); 
 		assertEquals(true, true);
